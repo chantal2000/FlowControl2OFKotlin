26b40bc9d42441d4104603808cf77e0e2487018b
chantal2000 <nniyonkuruchantal@gmail.com>
2021-04-20 22:25:15 +0300

    Flow Control

--- src/Flow_control2.kt
+++ src/Flow_control2.kt
@@ -1,55 +1,55 @@
 fun main() {
- println(isEven(7))
-    println(isEven(8))
-    println(E("Banana"))
-    println(S(arrayOf("Chantal","Emma","Peter","Jeanne","Mama")))
+// println(isEven(7))
+//    println(isEven(8))
+//    println(E("Banana"))
+//    println(S(arrayOf("Chantal","Emma","Peter","Jeanne","Mama")))
     var prod = Product("pen", 40, 500, "gloceries")
-    println(prod.P("pen"))
+    prod.P("pen")
     var prod2 = Product("Book", 10, 600, "hygiene")
-    println(prod2.P("Book"))
+    prod2.P("Book")
   var prod3 = Product("Chair", 20, 300, "other")
-    println(prod3.P("Chair"))
+    prod3.P("Chair")
 }
-fun isEven(num:Int):Boolean{
- if(num%2==0){
-    return true
-  }
-    else{
-        return false
-  }
-}
+//fun isEven(num:Int):Boolean{
+// if(num%2==0){
+//    return true
+//  }
+//    else{
+//        return false
+//  }
+//}
 
     data class Product(var name:String,var weight:Int,var price:Int,var category:String ){
 fun P(product:String) {
     when (product) {
         "pen" -> println("Pen = gloceries")
         "Book" -> println("Book = Hygienne")
-        "Chair" -> println("Chair = other")
+        else -> println("Chair = other")
     }
 }
 }
-    fun E(s: String): String {
-        var x = " "
-        for (i in s) {
-            var x = " "
-            if (i !== 'i' || i !== 'u' || i !== '0' || i !== 'a' || i !== 'e') {
-                return x.plus(i)
-            }
-        }
-    }
-
-
-    fun S(names: Array<String>): Array<String> {
-        var c =  ""
-        for (name in names) {
-            if (name.length % 2 == 0) {
-                c += 1
-
-            }
-
-            return name.plus(c)
-        }
-    }
-}
+//    fun E(s: String): String {
+//        var x = " "
+//        for (i in s) {
+//            var x = " "
+//            if (i !== 'i' || i !== 'u' || i !== '0' || i !== 'a' || i !== 'e') {
+//                return x.plus(i)
+//            }
+//        }
+//    }
+//
+//
+//    fun S(names: Array<String>): Array<String> {
+//        var c =  ""
+//        for (name in names) {
+//            if (name.length % 2 == 0) {
+//                c += 1
+//
+//            }
+//
+//            return name.plus(c)
+//        }
+//    }
+//}
 
 

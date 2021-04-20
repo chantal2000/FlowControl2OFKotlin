b292452b985c977243558c34fa267333ef1a3623
chantal2000 <nniyonkuruchantal@gmail.com>
2021-04-20 22:27:56 +0300

    Flow Control

--- src/Flow_control2.kt
+++ src/Flow_control2.kt
@@ -1,8 +1,8 @@
 fun main() {
-// println(isEven(7))
-//    println(isEven(8))
-//    println(E("Banana"))
-//    println(S(arrayOf("Chantal","Emma","Peter","Jeanne","Mama")))
+ println(isEven(7))
+    println(isEven(8))
+    println(E("Banana"))
+    println(S(arrayOf("Chantal","Emma","Peter","Jeanne","Mama")))
     var prod = Product("pen", 40, 500, "gloceries")
     prod.P("pen")
     var prod2 = Product("Book", 10, 600, "hygiene")
@@ -10,14 +10,14 @@ fun main() {
   var prod3 = Product("Chair", 20, 300, "other")
     prod3.P("Chair")
 }
-//fun isEven(num:Int):Boolean{
-// if(num%2==0){
-//    return true
-//  }
-//    else{
-//        return false
-//  }
-//}
+fun isEven(num:Int):Boolean{
+ if(num%2==0){
+    return true
+  }
+    else{
+        return false
+  }
+}
 
     data class Product(var name:String,var weight:Int,var price:Int,var category:String ){
 fun P(product:String) {
@@ -28,28 +28,29 @@ fun P(product:String) {
     }
 }
 }
-//    fun E(s: String): String {
-//        var x = " "
-//        for (i in s) {
-//            var x = " "
-//            if (i !== 'i' || i !== 'u' || i !== '0' || i !== 'a' || i !== 'e') {
-//                return x.plus(i)
-//            }
-//        }
-//    }
-//
-//
-//    fun S(names: Array<String>): Array<String> {
-//        var c =  ""
-//        for (name in names) {
-//            if (name.length % 2 == 0) {
-//                c += 1
-//
-//            }
-//
-//            return name.plus(c)
-//        }
-//    }
-//}
+    fun E(s: String): String {
+        var x = " "
+        for (i in s) {
+            var x = " "
+            if (i !== 'i' || i !== 'u' || i !== '0' || i !== 'a' || i !== 'e') {
+                return x.plus(i)
+            }
+        }
+    }
+
+
+    fun S(names: Array<String>): Array<String> {
+        var c =  ""
+        for (name in names) {
+            if (name.length % 2 == 0) {
+                c += 1
+
+            }
+
+            return name.plus(c)
+        }
+    }
+}
+
 
 

bb74655f6771a98684cf5efa79950adfdccffd97
chantal2000 <nniyonkuruchantal@gmail.com>
2021-04-20 22:15:06 +0300

    Flow Control

--- src/Flow_control2.kt
+++ src/Flow_control2.kt
@@ -1,18 +1,18 @@
 fun main() {
-// println(isEven(7))
-//    println(isEven(8))
+ println(isEven(7))
+    println(isEven(8))
     println(E("Banana"))
     println(S(arrayOf("Chantal","Emma","Peter","Jeanne","Mama")))
     var prod = Product("pen", 40, 500, "gloceries")
-    println(prod.P("Chair"))
+    println(prod.P("pen"))
     var prod2 = Product("Book", 10, 600, "hygiene")
-    println(prod2.P("Chair"))
+    println(prod2.P("Book"))
   var prod3 = Product("Chair", 20, 300, "other")
     println(prod3.P("Chair"))
 }
 fun isEven(num:Int):Boolean{
  if(num%2==0){
-/     return true
+    return true
   }
     else{
         return false
@@ -22,36 +22,34 @@ fun isEven(num:Int):Boolean{
     data class Product(var name:String,var weight:Int,var price:Int,var category:String ){
 fun P(product:String) {
     when (product) {
-        "pen" -> println("Pen = glocerie")
-        "Book" -> println("Book = glocerie")
+        "pen" -> println("Pen = gloceries")
+        "Book" -> println("Book = Hygienne")
         "Chair" -> println("Chair = other")
     }
 }
 }
     fun E(s: String): String {
-//        for (i in s) {
-//            if ([i.isLetter(){
-//                    return i
-//                }
-//        }
-//    }
+        var x = " "
         for (i in s) {
+            var x = " "
             if (i !== 'i' || i !== 'u' || i !== '0' || i !== 'a' || i !== 'e') {
-                return i
+                return x.plus(i)
             }
         }
     }
 
+
     fun S(names: Array<String>): Array<String> {
-        var c = 0
+        var c =  ""
         for (name in names) {
             if (name.length % 2 == 0) {
                 c += 1
 
             }
-        }
-            return name
 
+            return name.plus(c)
+        }
     }
 }
+
 

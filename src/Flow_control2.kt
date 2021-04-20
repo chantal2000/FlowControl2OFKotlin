8a97f23fd54663c08406762734e5da4fda8b6411
chantal2000 <nniyonkuruchantal@gmail.com>
2021-04-20 21:53:05 +0300

    Flow Control ASS@

---
+++ src/Flow_control2.kt
@@ -0,0 +1,57 @@
+fun main() {
+// println(isEven(7))
+//    println(isEven(8))
+    println(E("Banana"))
+    println(S(arrayOf("Chantal","Emma","Peter","Jeanne","Mama")))
+    var prod = Product("pen", 40, 500, "gloceries")
+    println(prod.P("Chair"))
+    var prod2 = Product("Book", 10, 600, "hygiene")
+    println(prod2.P("Chair"))
+  var prod3 = Product("Chair", 20, 300, "other")
+    println(prod3.P("Chair"))
+}
+fun isEven(num:Int):Boolean{
+ if(num%2==0){
+/     return true
+  }
+    else{
+        return false
+  }
+}
+
+    data class Product(var name:String,var weight:Int,var price:Int,var category:String ){
+fun P(product:String) {
+    when (product) {
+        "pen" -> println("Pen = glocerie")
+        "Book" -> println("Book = glocerie")
+        "Chair" -> println("Chair = other")
+    }
+}
+}
+    fun E(s: String): String {
+//        for (i in s) {
+//            if ([i.isLetter(){
+//                    return i
+//                }
+//        }
+//    }
+        for (i in s) {
+            if (i !== 'i' || i !== 'u' || i !== '0' || i !== 'a' || i !== 'e') {
+                return i
+            }
+        }
+    }
+
+    fun S(names: Array<String>): Array<String> {
+        var c = 0
+        for (name in names) {
+            if (name.length % 2 == 0) {
+                c += 1
+                // val name = "Mary"
+            }
+        }
+            return name
+
+    }
+}
+
